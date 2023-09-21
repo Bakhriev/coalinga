60eee0acc6a4585413837e6fc51dd71a717a3de6
Bakhriev <legendofpain5112@gmail.com>
2023-09-22 00:10:14 +0500

    осталось подключить слайдер

---
+++ src/assets/js/functions/swiper.js
@@ -0,0 +1,14 @@
+// import Swiper JS
+import Swiper from 'swiper'
+import {Navigation} from 'swiper/modules'
+
+// import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+// import Swiper and modules styles
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+	modules: [Navigation],
+})
