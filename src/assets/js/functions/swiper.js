43e4ccbf90852a3264d88b419374e4f0e5daf4c9
Bakhriev <legendofpain5112@gmail.com>
2023-09-22 11:51:11 +0500

    form

--- src/assets/js/functions/swiper.js
+++ src/assets/js/functions/swiper.js
@@ -8,7 +8,34 @@ import 'swiper/css/navigation'
 
 // import Swiper and modules styles
 
-// init Swiper:
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.products-swiper', {
+	direction: 'horizontal',
+	spaceBetween: 20,
+
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		540: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 1,
+		},
+		920: {
+			slidesPerView: 2,
+		},
+		1200: {
+			slidesPerView: 3,
+		},
+	},
+
+	// Modules
 	modules: [Navigation],
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.product-next-btn',
+		prevEl: '.product-prev-btn',
+	},
 })
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,5 @@
 import {burgerMenu} from './functions/burger'
 import {dropdownInit} from './functions/dropdown'
+import {swiper} from './functions/swiper'
 burgerMenu()
 dropdownInit()
