8a7922c1aa22ae2cfb1bfa39b9132f93b4f9767f
Bakhriev <legendofpain5112@gmail.com>
2023-09-24 11:47:13 +0500

    last

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -132,16 +132,16 @@ const burgerMenu = () => {
 
 const dropdownInit = () => {
 	const allDropdowns = document.querySelectorAll('[data-dropdown]')
-	// if (window.innerWidth > 1024) {
-	// 	allDropdowns.forEach(drp => {
-	// 		drp.addEventListener('mouseover', () => {
-	// 			drp.classList.add('active')
-	// 		})
-	// 		drp.addEventListener('mouseleave', () => {
-	// 			drp.classList.remove('active')
-	// 		})
-	// 	})
-	// }
+	if (window.innerWidth > 1024) {
+		allDropdowns.forEach(drp => {
+			drp.addEventListener('mouseover', () => {
+				drp.classList.add('active')
+			})
+			drp.addEventListener('mouseleave', () => {
+				drp.classList.remove('active')
+			})
+		})
+	}
 
 	if (window.innerWidth > 992.98) {
 		document.addEventListener('click', e => {
